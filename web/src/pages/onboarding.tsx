af12bb206410d4a474b4533b1f7e933aa8b8b77e
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-02-06 05:36:35 -0800

    acernative ui

---
+++ web/src/pages/onboarding.tsx
@@ -0,0 +1,9 @@
+const Onboarding = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+        <h1 className="italic text-center font-bold text-[3rem] logo text-white">Finder</h1>
+    </div>
+  )
+}
+
+export default Onboarding;
