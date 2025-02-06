e58b0ba37781a9d062bddbd06004d4d396d7e379
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-02-06 06:03:34 -0800

    push

--- web/src/pages/onboarding.tsx
+++ web/src/pages/onboarding.tsx
@@ -1,7 +1,10 @@
+import TabMenu from "../components/ui/tab";
+
 const Onboarding = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-        <h1 className="italic text-center font-bold text-[3rem] logo text-white">Finder</h1>
+        <h1 className="logo font-bold italic text-white text-[2rem] pt-[50px]">Finder</h1>
+        <TabMenu />
     </div>
   )
 }
